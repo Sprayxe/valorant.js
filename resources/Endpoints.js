edd19f946dd606e8caf88d05a5d300126a1daf1d
Sprayxe <52536239+Sprayxe@users.noreply.github.com>
2020-08-09 19:25:47 +0200

    Update Endpoints.js

--- resources/Endpoints.js
+++ resources/Endpoints.js
@@ -1,5 +1,6 @@
 module.exports = Object.freeze({
   AUTH: "https://auth.riotgames.com",
   ENTITLEMENTS: "https://entitlements.auth.riotgames.com",
+  BASE: "https://pd.na.a.pvp.net",
   //more coming soon
 });
