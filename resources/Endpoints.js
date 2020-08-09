f337e08572eaf7163a821f245e6076980798d306
Sprayxe <52536239+Sprayxe@users.noreply.github.com>
2020-08-09 19:43:37 +0200

    Update Endpoints.js

--- resources/Endpoints.js
+++ resources/Endpoints.js
@@ -1,9 +1,19 @@
 module.exports = Object.freeze({
+
   AUTH: "https://auth.riotgames.com",
+
   ENTITLEMENTS: "https://entitlements.auth.riotgames.com",
+
   NABASE: "https://pd.na.a.pvp.net",
+
   EUBASE: "https://pd.eu.a.pvp.net",
+
+  ASIABASE: "https://pd.ap.a.pvp.net",
+
   NASHARED: "https://shared.na.a.pvp.net",
+
   EUSHARED: "https://shared.eu.a.pvp.net",
-  //more coming soon
+
+  ASIASHARED: "https://shared.ap.a.pvp.net",
+
 });
