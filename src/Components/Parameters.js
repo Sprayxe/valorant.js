359a26af29937ad4bb61c92351620968d209b8db
Sprayxe <52536239+Sprayxe@users.noreply.github.com>
2020-08-11 21:40:21 +0200

    Update Parameters.js

--- src/Components/Parameters.js
+++ src/Components/Parameters.js
@@ -3,25 +3,26 @@ module.exports = {
      * @param {object} config - The object to check for possible error
      * @param {string} type - Determines if its the Valorant Client constructor or a Request
      */
-    checkParams: function(config, type) {
+    checkParams: function(data, type) {
         const ValorantError = require("./Error");
 
         // Check for right Valorant Client config
         if(type === "client") {
-            // Not provided Errors
-            if(!config.password) new ValorantError("Account password has not been provided!", "reference");
-            if(!config.email) new ValorantError("Account email has not been provided!", "reference");
-            if(!config.region) new ValorantError("Account region has not been provided!", "reference");
+            // ReferenceErrors
+            if(!data.password) new ValorantError("Account password has not been provided!", "reference");
+            if(!data.email) new ValorantError("Account email has not been provided!", "reference");
+            if(!data.region) new ValorantError("Account region has not been provided!", "reference");
 
             // TypeErrors
-            if(typeof config.password !== "string") new ValorantError("Account password is not a string!", "type");
-            if(typeof config.email !== "string") new ValorantError("Account email is not a string!", "type");
-            if(typeof config.region !== "object") new ValorantError("Account region is not an object! Use the Region enums.", "type");
+            if(typeof data.password !== "string") new ValorantError("Account password is not a string!", "type");
+            if(typeof data.email !== "string") new ValorantError("Account email is not a string!", "type");
+            if(typeof data.region !== "object") new ValorantError("Account region is not an object! Use the Region enums.", "type");
         }
         
         // Check for auth & account before sending request
         if(type === "request") {
-          // code
+          if(data.Authorization === null || !data.Authorization.fullToken || !data.Authorization.RSOToken) new ValorantError("You are not properly authorized! Please login first.", "syntax");
+          if(data.account === null || !data.account.id) new ValorantError("You are not properly authorized. Please login first.", "syntax");
         }
     }
 };
