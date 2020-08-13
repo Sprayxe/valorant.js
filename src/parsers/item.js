62382d91909f3b1f688948f8ff1b673bb9312066
Sprayxe <52536239+Sprayxe@users.noreply.github.com>
2020-08-13 08:57:31 +0200

    Update item.js

---
+++ src/parsers/item.js
@@ -0,0 +1,17 @@
+class ItemParser {
+  /**
+  * Parses the valorant storefront item's
+  * @param data {object} Data to parse
+  * @returns {object} parsed data
+  */
+  constructor(data) {
+    this.data = data;
+  };
+
+  parse() {
+    
+  //code here lol
+  }
+
+};
+module.exports = ItemParser;
