348fb42cc1a2b7f25d5aef13046df32d6cd46aa6
Sprayxe <52536239+Sprayxe@users.noreply.github.com>
2020-08-13 08:56:08 +0200

    Update mmr.js

--- src/parsers/mmr.js
+++ src/parsers/mmr.js
@@ -1,1 +1,18 @@
 
+class MMRParser {
+  /**
+  * Parses the valorant player mmr
+  * @param data {object} Data to parse
+  * @returns {object} parsed data
+  */
+  constructor(data) {
+    this.data = data;
+  };
+
+  parse() {
+    
+  //code here lol
+  }
+
+};
+module.exports = MMRParser;
