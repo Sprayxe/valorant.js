c58b3acb9c1947aa5a9df4883805378193f03309
Sprayxe <52536239+Sprayxe@users.noreply.github.com>
2020-08-14 12:52:29 +0200

    Update messages.js

--- src/main/debug/messages.js
+++ src/main/debug/messages.js
@@ -14,12 +14,13 @@ module.exports = {
     ACCOUNT_GETWALLET_SUCCESS:"Got account wallet!",
     ACCOUNT_GETWALLET_FAIL:"Failed to get account wallet!",
 
-
+   
     // Match API: History
     MATCH_GETHISTORY_START:"Getting match history...",
     MATCH_GETHISTORY_SUCCESS:"Got match history!",
     MATCH_GETHISTORY_FAIL:"Failed to get match history",
 
     MATCH_GETHISTORY_PARSE:"Parsing match history...",
-    MATCH_GETHISTORY
+    MATCH_GETHISTORY_PARSESUCCESS:"Parsed match history!",
+    MATCH_GEHISTORY_PARSEFAIL:"Failed to parse match history!",
 }
