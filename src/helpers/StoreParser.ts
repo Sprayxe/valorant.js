fe482eea88c0400231c4197c0db437a379147d22
Sprayxe <sprayxe.marcel@gmail.com>
2021-04-20 11:04:44 +0200

    Fixed error when shopData is 'null'

--- src/helpers/StoreParser.ts
+++ src/helpers/StoreParser.ts
@@ -71,7 +71,8 @@ export class StoreParser {
                 skins.push({
                     name: it ? it.skin.displayName : "Unknown",
                     id: s,
-                    cost: { id: "85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741", name: "Valorant Points", amount: it.entry.shopData.cost }
+                    cost: { id: "85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741", name: "Valorant Points",
+                        amount: it.entry.shopData ? it.entry.shopData.cost : -1 }
                 })
             })
         }
